e5bf0ee05c53bb3006348211e12380439ed049c0
Keiwen <romainwald@hotmail.com>
2017-07-28 11:02:56 +0200

    fix issue with npm package

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // import EnhancedInput from './EnhancedInput.vue'
-import EnhancedCheck from './components/EnhancedCheck.vue'
-import EnhancedCheckGroup from './components/EnhancedCheckGroup.vue'
-import EnhancedCheckRadio from './components/EnhancedCheckRadio.vue'
+const EnhancedCheck = require('./components/EnhancedCheck.vue')
+const EnhancedCheckGroup = require('./components/EnhancedCheckGroup.vue')
+const EnhancedCheckRadio = require('./components/EnhancedCheckRadio.vue')
 // import EnhancedToggle from './EnhancedToggle.vue'
 
 module.exports = {

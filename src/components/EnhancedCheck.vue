aa67b4a5fd07a9966cfe9f1b3a5493f736ca353d
Keiwen <keiwen.mk@gmail.com>
2025-01-07 21:42:15 +0100

    feat: VUE-2 EnhancedCheck vue3
    enhanced check

--- src/components/EnhancedCheck.vue
+++ src/components/EnhancedCheck.vue
@@ -1,87 +1,55 @@
-<template>
-    <div class="enhancedCheck" :class="computedClass">
-        <div>
-            <input type="checkbox" :id="generatedId" :name="name" :value="value" @change="inputChange()" :disabled="disabled" v-model="inputModel">
-            <label :for="generatedId">{{ label }}</label>
-        </div>
-    </div>
-</template>
+<script setup>
+import { ref, computed, onMounted, defineProps, defineEmits } from 'vue'
 
-<script>
+const props = defineProps({
+  modelValue: Boolean,
+  label: { type: String, required: true },
+  id: { type: String, default: '' },
+  checked: { default: false },
+  name: { default: '' },
+  value: { default: '' },
+  subClass: { type: String, default: 'default' },
+  disabled: { type: Boolean, default: false },
+  rounded: { type: Boolean, default: false },
+  animate: { type: Boolean, default: false }
+})
 
-export default {
-  name: 'EnhancedCheck',
-  model: {
-    prop: 'checked'
-  },
-  props: {
-    label: {
-      type: String,
-      required: true
-    },
-    id: {
-      type: String,
-      default: ''
-    },
-    checked: {
-      default: false
-    },
-    name: {
-      default: ''
-    },
-    value: {
-      default: ''
-    },
-    subClass: {
-      type: String,
-      default: 'default'
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    rounded: {
-      type: Boolean,
-      default: false
-    },
-    animate: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data () {
-    return {
-      inputModel: this.checked,
-      generatedId: ''
-    }
-  },
-  mounted () {
-    if (this.id === '') {
-      this.generatedId = 'enhancedCheck_' + Math.random().toString(36).substr(2, 9)
-    } else {
-      this.generatedId = this.id
-    }
-  },
-  watch: {
-    checked: function (newValue) {
-      this.inputModel = newValue
-    }
-  },
-  computed: {
-    computedClass () {
-      let computedClass = 'enhancedCheck-' + this.subClass
-      if (this.rounded) computedClass += ' enhancedCheck-rounded'
-      if (this.animate) computedClass += ' enhancedCheck-animate'
-      return computedClass
-    }
-  },
-  methods: {
-    inputChange () {
-      this.$emit('input', this.inputModel)
-    }
+const inputModel = ref(false)
+const generatedId = ref('')
+
+const emit = defineEmits(['update:modelValue', 'input'])
+
+// computed
+const computedClass = computed(() => {
+  let computedClass = 'enhancedCheck-' + props.subClass
+  if (props.rounded) computedClass += ' enhancedCheck-rounded'
+  if (props.animate) computedClass += ' enhancedCheck-animate'
+  return computedClass
+})
+
+onMounted(async () => {
+  if (props.id === '') {
+    generatedId.value = 'enhancedCheck_' + Math.random().toString(36).substr(2, 9)
+  } else {
+    generatedId.value = props.id
   }
+})
+
+// methods
+const inputChange = () => {
+  emit('update:modelValue', inputModel.value)
 }
+
 </script>
+
+<template>
+  <div class="enhancedCheck" :class="computedClass">
+    <div>
+      <input type="checkbox" :id="generatedId" :name="name" :value="value" @change="inputChange()" :disabled="disabled" v-model="inputModel" :checked="modelValue">
+      <label :for="generatedId">{{ label }}</label>
+    </div>
+  </div>
+</template>
 
 <style lang="scss" scoped>
     @import '../styles/common.scss';

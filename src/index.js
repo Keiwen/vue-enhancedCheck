436e6a0ffbb7bca4bc1c84246187cd7b66a8c01f
Keiwen <keiwen.mk@gmail.com>
2025-01-07 19:45:32 +0100

    feat: VUE-2 EnhancedCheck vue3
    change config

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Component imports
+import EnhancedCheck from './components/EnhancedCheck.vue'
+import EnhancedCheckGroup from './components/EnhancedCheckGroup.vue'
+import EnhancedCheckRadio from './components/EnhancedCheckRadio.vue'
+import EnhancedToggle from './components/EnhancedToggle.vue'
+
+// Default export for global usage
+const install = (app) => {
+  app.component('enhanced-check', EnhancedCheck)
+  app.component('enhanced-check-group', EnhancedCheckGroup)
+  app.component('enhanced-check-radio', EnhancedCheckRadio)
+  app.component('enhanced-toggle', EnhancedToggle)
+}
+export default install
+
+// Named export for specific usage
+export { install, EnhancedCheck, EnhancedCheckGroup, EnhancedCheckRadio, EnhancedToggle }

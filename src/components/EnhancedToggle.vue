3ccf40f3032ba8a4d0a572889ec2c0286403a387
Keiwen <keiwen.mk@gmail.com>
2025-01-08 08:54:30 +0100

    feat: VUE-2 EnhancedCheck vue3
    enhanced toggle

--- src/components/EnhancedToggle.vue
+++ src/components/EnhancedToggle.vue
@@ -1,141 +1,114 @@
-<template>
-    <div class="enhancedCheck enhancedToggle" :class="computedClass" :style="style">
-        <label :for="generatedId">
-            <input type="checkbox" :id="generatedId" :name="name" :value="value" @change="inputChange()" :disabled="disabled" v-model="inputModel" />
+<script setup>
+import { ref, computed, watch, onMounted, defineProps, defineEmits } from 'vue'
 
-            <span class="enhancedToggle-label labelOn" ref="toggleLabelOn">{{ labelOn }}</span>
-            <span class="enhancedToggle-switch"></span>
-            <span class="enhancedToggle-label labelOff" ref="toggleLabelOff">{{ labelOff }}</span>
-        </label>
-    </div>
-</template>
-
-<script>
+const props = defineProps({
+  modelValue: Boolean,
+  id: { type: String, default: '' },
+  checked: { default: false },
+  labelOn: { type: String, default: 'On' },
+  labelOff: { type: String, default: 'Off' },
+  name: { default: '' },
+  value: { default: '' },
+  styleOn: { type: String, default: 'primary' },
+  styleOff: { type: String, default: 'default' },
+  disabled: { type: Boolean, default: false },
+  rounded: { type: Boolean, default: false }
+})
 
 const DEFAULT_WIDTH = 50
 const SWITCH_WIDTH = 30
 const MIN_WIDTH = 50
 const HORIZONTAL_PADDING = 10
 
-export default {
-  name: 'EnhancedToggle',
-  model: {
-    prop: 'checked'
-  },
-  props: {
-    id: {
-      type: String,
-      default: ''
-    },
-    checked: {
-      default: false
-    },
-    labelOn: {
-      type: String,
-      default: 'On'
-    },
-    labelOff: {
-      type: String,
-      default: 'Off'
-    },
-    name: {
-      default: ''
-    },
-    value: {
-      default: ''
-    },
-    styleOn: {
-      type: String,
-      default: 'primary'
-    },
-    styleOff: {
-      type: String,
-      default: 'default'
-    },
-    disabled: {
-      type: Boolean,
-      default: false
-    },
-    rounded: {
-      type: Boolean,
-      default: false
-    }
-  },
-  data () {
-    return {
-      style: {},
-      initLabelMaxWidth: 0,
-      initLabelMaxChar: 0,
-      labelMaxWidth: 0,
-      labelMaxChar: 0,
-      inputModel: this.checked,
-      generatedId: ''
-    }
-  },
-  watch: {
-    checked: function (newValue) {
-      this.inputModel = newValue
-    },
-    labelOn: function (newValue) {
-      this.updateLabels()
-    },
-    labelOff: function (newValue) {
-      this.updateLabels()
-    }
-  },
-  mounted () {
-    if (this.id === '') {
-      this.generatedId = 'enhancedToggle_' + Math.random().toString(36).substr(2, 9)
-    } else {
-      this.generatedId = this.id
-    }
-    this.initLabelMaxWidth = this.getLabelMaxWidth()
-    this.initLabelMaxChar = this.getLabelMaxChar()
-    this.labelMaxWidth = this.initLabelMaxWidth
-    this.labelMaxChar = this.initLabelMaxChar; this.computeStyle()
-  },
-  computed: {
-    computedClass () {
-      let computedClass = 'enhancedCheck-' + this.styleOff
-      if (this.inputModel) {
-        computedClass = 'enhancedCheck-' + this.styleOn
-        computedClass += ' enhancedCheck-checked'
-      }
-      if (this.disabled) computedClass += ' enhancedCheck-disabled'
-      if (this.rounded) computedClass += ' enhancedCheck-rounded'
-      return computedClass
-    }
-  },
-  methods: {
-    inputChange () {
-      this.$emit('input', this.inputModel)
-    },
-    getLabelMaxWidth () {
-      if (typeof this.$refs.toggleLabelOn === 'undefined') return DEFAULT_WIDTH
-      return Math.max(this.$refs.toggleLabelOn.getBoundingClientRect().width, this.$refs.toggleLabelOff.getBoundingClientRect().width) - HORIZONTAL_PADDING
-    },
-    getLabelMaxChar () {
-      return Math.max(this.labelOn.length, this.labelOff.length)
-    },
-    updateLabels () {
-      const newMaxChar = this.getLabelMaxChar()
-      if (newMaxChar !== this.labelMaxChar) {
-        this.labelMaxWidth = this.initLabelMaxWidth / this.initLabelMaxChar * newMaxChar
-        if (this.labelMaxWidth < MIN_WIDTH) this.labelMaxWidth = MIN_WIDTH
-        this.computeStyle()
-      }
-    },
-    computeStyle () {
-      this.style = {
-        '--labelWidth': this.labelMaxWidth + 'px',
-        '--switchWidth': SWITCH_WIDTH + 'px',
-        '--switchPos': (this.labelMaxWidth + HORIZONTAL_PADDING) + 'px',
-        '--toggleWidth': (this.labelMaxWidth + SWITCH_WIDTH / 2 + HORIZONTAL_PADDING) + 'px'
-      }
-    }
+const style = ref({})
+const initLabelMaxWidth = ref(0)
+const initLabelMaxChar = ref(0)
+const labelMaxWidth = ref(0)
+const labelMaxChar = ref(0)
+const inputModel = ref(false)
+const generatedId = ref('')
+const toggleLabelOn = ref(null)
+const toggleLabelOff = ref(null)
+
+const emit = defineEmits(['update:modelValue'])
+
+onMounted(async () => {
+  if (props.id === '') {
+    generatedId.value = 'enhancedToggle_' + Math.random().toString(36).substr(2, 9)
+  } else {
+    generatedId.value = props.id
+  }
+  initLabelMaxWidth.value = getLabelMaxWidth()
+  initLabelMaxChar.value = getLabelMaxChar()
+  labelMaxWidth.value = initLabelMaxWidth.value
+  labelMaxChar.value = initLabelMaxChar.value
+  inputModel.value = props.modelValue
+  computeStyle()
+})
+
+watch(() => props.modelValue, (newValue, oldValue) => {
+  if (newValue !== inputModel.value) inputModel.value = newValue
+})
+watch(() => props.labelOn, (newValue, oldValue) => {
+  updateLabels()
+})
+watch(() => props.labelOff, (newValue, oldValue) => {
+  updateLabels()
+})
+
+// computed
+const computedClass = computed(() => {
+  let computedClass = 'enhancedCheck-' + props.styleOff
+  if (inputModel.value) {
+    computedClass = 'enhancedCheck-' + props.styleOn
+    computedClass += ' enhancedCheck-checked'
+  }
+  if (props.rounded) computedClass += ' enhancedCheck-rounded'
+  if (props.animate) computedClass += ' enhancedCheck-animate'
+  return computedClass
+})
+
+// methods
+const inputChange = () => {
+  emit('update:modelValue', inputModel.value)
+}
+const getLabelMaxWidth = () => {
+  if (typeof toggleLabelOn.value === 'undefined') return DEFAULT_WIDTH
+  return Math.max(toggleLabelOn.value.getBoundingClientRect().width, toggleLabelOff.value.getBoundingClientRect().width) - HORIZONTAL_PADDING
+}
+const getLabelMaxChar = () => {
+  return Math.max(props.labelOn.length, props.labelOff.length)
+}
+const updateLabels = () => {
+  const newMaxChar = getLabelMaxChar()
+  if (newMaxChar !== labelMaxChar.value) {
+    labelMaxWidth.value = initLabelMaxWidth.value / initLabelMaxChar.value * newMaxChar
+    if (labelMaxWidth.value < MIN_WIDTH) labelMaxWidth.value = MIN_WIDTH
+    computeStyle()
   }
 }
+const computeStyle = () => {
+  style.value = {
+    '--labelWidth': labelMaxWidth.value + 'px',
+    '--switchWidth': SWITCH_WIDTH + 'px',
+    '--switchPos': (labelMaxWidth.value + HORIZONTAL_PADDING) + 'px',
+    '--toggleWidth': (labelMaxWidth.value + SWITCH_WIDTH / 2 + HORIZONTAL_PADDING) + 'px'
+  }
+}
+
 </script>
+
+<template>
+  <div class="enhancedCheck enhancedToggle" :class="computedClass" :style="style">
+    <label :for="generatedId">
+      <input type="checkbox" :id="generatedId" :name="name" :value="value" @change="inputChange()" :disabled="disabled" v-model="inputModel" :checked="modelValue" />
+
+      <span class="enhancedToggle-label labelOn" ref="toggleLabelOn">{{ labelOn }}</span>
+      <span class="enhancedToggle-switch"></span>
+      <span class="enhancedToggle-label labelOff" ref="toggleLabelOff">{{ labelOff }}</span>
+    </label>
+  </div>
+</template>
 
 <style lang="scss" scoped>
     @import '../styles/common.scss';
